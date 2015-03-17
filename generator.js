9fc553fb77e0ea4ee24baadf2c32257ca097dd85
marcioaguiar <aguiar.marcio+github@gmail.com>
2015-03-17 15:46:24 -0300

    Better configuration in separate files

--- generator.js
+++ generator.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var _  = require('lodash');
 
-function start(configFile, output) {
+function start(serverFile, writersFile, queriesFile, output) {
 
 	if (!output) {
 		console.log('no output defined');
@@ -9,13 +9,15 @@ function start(configFile, output) {
 	}
 
 	var fileOpts = { encoding: 'UTF-8' };
-	var configuration = JSON.parse(fs.readFileSync(configFile, fileOpts));
+	var inputServers = JSON.parse(fs.readFileSync(serverFile, fileOpts));
+	var queries = JSON.parse(fs.readFileSync(queriesFile, fileOpts));
+	var writers = JSON.parse(fs.readFileSync(writersFile, fileOpts));
 
 	var servers = [];
 
-	_.forIn(configuration.servers, function(server, serverName) {
+	_.forIn(inputServers, function(server, serverName) {
 		console.log('configuring server', serverName);
-		servers.push(configureServer(server, configuration));
+		servers.push(configureServer(server, queries, writers));
 	});
 
 	var content = JSON.stringify({ servers: servers });
@@ -23,27 +25,40 @@ function start(configFile, output) {
 
 }
 
-function configureServer(server, configuration) {
+function configureServer(server, queries, writers) {
 	var actualQueries = [];
 
 	server.queries.forEach(function(query) {
 
-		var q = _.clone(configuration.queries[query]);
+		if (!queries[query]) {
+			console.log('query not found:', query);
+			return;
+		}
+
+		var q = _.clone(queries[query]);
+
 		actualQueries.push(q);
+
 		q.outputWriters = [];
 		server.writers.forEach(function(writer) {
-			q.outputWriters.push(configuration.outputWriters[writer]);
+			if (!writers[writer]) {
+				console.log('writer not found:', writer);
+			}
+			q.outputWriters.push(writers[writer]);
 		});
+
 	});
+
+	delete server.writers;
 
 	server.queries = actualQueries;
 
 	return server;
 };
 
-if (process.argv < 4) {
-	console.log('Usage: node generator.js [input] [output]');
+if (process.argv < 6) {
+	console.log('Usage: node generator.js [servers-file] [writers-file] [queries-file] [output]');
 	return;
 }
 
-start(process.argv[2], process.argv[3]);
+start(process.argv[2], process.argv[3], process.argv[4], process.argv[5]);
